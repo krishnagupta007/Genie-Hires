e263e70b1289f759ea7ca3af4528f75cf43adbce
krishnagupta007 <121493666+krishnagupta007@users.noreply.github.com>
2024-04-21 19:47:10 +0530

    Create EnvironmentVariables.js

---
+++ Backend/EnvironmentVariables.js
@@ -0,0 +1,17 @@
+const REACT_APP_ADMIN="admin"
+const REACT_APP_ADMIN_PASSWORD="admin"
+const REACT_APP_ADMIN_AUTH="shbdfgkhyebaskgiirfgbii24u53vzqsf"
+const REACT_APP_EMAIL_USER="geniehiresinc@gmail.com"
+const REACT_APP_EMAIL_PASSWORD="glyxgoigcaqliapq"
+const REACT_APP_JWTPRIVATEKEY="w5G)2w*^<d!pT3@#dslkjfb&8h9$ewfvyrwifv"
+
+// // user: "2205101110030@paruluniversity.ac.in",
+// // pass: "gmapgtyayuxalart"
+// // user: "kgedits0407@gmail.com",
+// // pass: "keaskycgoveueawa"
+// user: "geniehiresinc@gmail.com",
+// pass: "glyxgoigcaqliapq"
+
+module.exports = {
+    REACT_APP_ADMIN, REACT_APP_ADMIN_PASSWORD,REACT_APP_ADMIN_AUTH,REACT_APP_EMAIL_USER,REACT_APP_EMAIL_PASSWORD,REACT_APP_JWTPRIVATEKEY
+}
